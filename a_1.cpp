564a1b2d411f780d72973a22d4a62573f3c2c249
aSafarpoor <alisafarpoor1995@gmail.com>
2019-05-23 08:23:31 +0430

    change in comparission

--- a_1.cpp
+++ a_1.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<string>
 using namespace std;
 
 #define  size  3 //1618
@@ -9,13 +11,10 @@ string s_arr[size+1];
 string s_arr_reverse[size+1];
 bool seen[size]={};
 
-void input(){
-	for(int i=0;i<size-1;i++){
+void input_strings(){
+	for(int i=0;i<size;i++){
 		cin>>s_arr[i];
 	}		
-	for(int i=0;i<size-1;i++){
-		cout<<endl<<s_arr[i];
-	}	
 }
  
 bool check(int n,int m,int start,int end ) { 
@@ -23,7 +22,7 @@ bool check(int n,int m,int start,int end ) {
 	string str_b=s_arr_reverse[m];
 	int length=end-start+1;
 	str_a=str_a.substr(start,length);//az start be andaze end :)
-	str_b=str_b.substr(start,length);
+	str_b=str_b.substr(size_of_each_string-length-start, length);
 
 	// cout<<start<<" "<<end<<" "<<(str_a==str_b)<<endl;
 	if(str_a==str_b)return true;
@@ -32,7 +31,7 @@ bool check(int n,int m,int start,int end ) {
 
 int find_common(int n,int m,int start,int end){
 	if(start>end) return end ;
-	// cout<<"hello";
+	
 	if (end==start){
 		bool c=check(n,m,start,end);
 		if(c)
@@ -58,24 +57,34 @@ void make_matrix(){
 	//matrix
 	for (int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
-			// if(i==j)
-			// 	matrix[i][j]=size_of_each_string-1;
-			// else{
-				
-					matrix[i][j]=find_common(i,j,0,size_of_each_string-1);
-		//	}
+			matrix[i][j]=find_common(i,j,0,size_of_each_string-1);		
 		}
 	}
 }
 
 string make_circular_genome(){
-	///
+	string genome="";
+	genome+=s_arr[0];
+	seen[0]=true;
+	int point=0;
+	int maximum=-1;
+	int next=-1;
+	for(int i=0;i<size;i++){
+		if(matrix[point][i]>maximum && seen[i]==false){
+			next=i;
+			maximum=matrix[point][i];
+		}
+	}
+	// genome///////////////////////////////////should be complete
+	// //////////////////////should to use backpropagate
+	point=next;
 }
 
 int main(){
-	input();
+	input_strings();
 	make_matrix();
 	make_circular_genome();
 }
+
 
 
